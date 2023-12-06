0b3b2f27e52da80b20e5d51df1319102a9fa29c1
Andrew Ashraf <andrewashraf698@gmail.com>
2023-12-06 11:10:31 +0200

    Adding Chatbot

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ChatBot
 A tiny teachable ChatBot that learns from the user input and make a response with high accuracy.
+
+It works by adding to it some questions and putting them into its Knowlodge base (database.json),
+if the bot doesn't find the question in his knowlodge base he will asks you to answer the question and he will automatically put the question with your answer in his database. 
--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from difflib import get_close_matches
 
 def load_json_file(file_path : str) -> dict:
     with open (file_path, 'r') as file:
-        data :dict = json.load(file)
+        data: dict = json.load(file)
     return data
 
 
 def saving_to_json (file_path : str, data : dict):
     with open (file_path , 'w') as file:
-        json.dump(file, data, indent = 4)
+        json.dump(data, file, indent = 4)
 
 
 def find_best_match (user_question : str, questions : list[str]) -> str | None:
-    best_match : str = get_close_matches(user_question, questions, n=1, cutoff=0.6)
-    return best_match if best_match else None
+    best_matches : list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return best_matches[0] if best_matches else None
 
 
 def get_answer(question : str, DataBase : dict) -> str | None :
@@ -24,4 +24,32 @@ def get_answer(question : str, DataBase : dict) -> str | None :
 
 
 def chatBot():
-    pass
+    dataBase : dict = load_json_file('TinyDB.json')
+    
+    while True:
+        userInput : str = input('You: ')
+    
+        if userInput.lower() == 'quit':
+            break
+
+        best_match : str | None = find_best_match(userInput, [q["question"] for q in dataBase["questions"]])
+
+        if best_match != None:
+            answer : str = get_answer(best_match, dataBase)
+            print (f'Beto: {answer}')
+        else:
+            print ('Beto: i don\'t know the answer, Can you teach me?')
+            teachable_answer : str = input('Please give me the answer or "skip" to skip: ')
+
+            if teachable_answer.lower() != "skip":
+                dataBase["questions"].append({"question" : userInput, "answer": teachable_answer })
+                saving_to_json('TinyDB.json', dataBase)
+                print ('Beto: Thank you, i have learned a new answer!')
+
+
+
+if __name__ == "__main__":
+    chatBot()
+
+
+
